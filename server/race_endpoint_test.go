854d75e0e96dc906f297c1cfaa5481daf166ddf8
David Kovacs <david.kovacs@transferwise.com>
2021-03-20 18:27:11 +0100

    Make race endpoint test better

--- server/race_endpoint_test.go
+++ server/race_endpoint_test.go
@@ -2,25 +2,24 @@ package main
 
 import (
 	"bytes"
-	"fmt"
-	"log"
 	"net/http"
 	"testing"
 	"time"
 
+	"github.com/dadikovi/race-timer/server/core"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestStartActiveGroup(t *testing.T) {
-	// given
-	clearTable()
-	createSegment("any-name")
-	segmentId := 1
 
-	// and group created (we expect with id 1)
-	groupReq, _ := http.NewRequest("POST", "/groups", bytes.NewBufferString(`{"segmentId": `+fmt.Sprint(segmentId)+`}`))
-	groupReq.Header.Set("Content-Type", "application/json")
-	executeRequest(groupReq)
+	// given
+	var createdSegment core.SegmentDto
+	var createdGroup core.GroupDto
+
+	// and
+	clearTable()
+	callCreateSegmentEndpoint("any-name", &createdSegment)
+	callCreateGroupEndpoint(int(createdSegment.Id), &createdGroup)
 
 	// when active group start endpoint is called
 	req, _ := http.NewRequest("PUT", "/groups/active", bytes.NewBufferString(``))
@@ -28,15 +27,13 @@ func TestStartActiveGroup(t *testing.T) {
 	response := executeRequest(req)
 
 	// then
-	log.Print(response.Body.String())
-
-	assert.Equal(t, http.StatusOK, response.Code, "Response code should be 200/OK")
+	assert.Equal(t, http.StatusOK, response.Code)
 
 	// when we get all the groups from the database
 	groupsFromDatabase := getGroupsFromDatabase()
 
 	// then there will be only our newly created element in it
-	assert.Equal(t, len(groupsFromDatabase), 1, "One record should be in the database")
+	assert.Equal(t, len(groupsFromDatabase), 1)
 	assert.True(t, time.Now().Sub(groupsFromDatabase[0].start) < 1000)
 }
 
