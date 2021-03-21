3205df10f9b9e656d55acd2a80d04032f3d28c6a
David Kovacs <david.kovacs@transferwise.com>
2021-03-21 15:05:57 +0100

    ParticipantFinished endpoint

--- server/participants_endpoint.go
+++ server/participants_endpoint.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/dadikovi/race-timer/server/core"
+	"github.com/gorilla/mux"
 )
 
 type registerParticipantRequest struct {
@@ -23,4 +25,17 @@ func (a *App) registerParticipant(w http.ResponseWriter, r *http.Request) {
 	respondWithServerError(w, err)
 
 	respondWithDto(w, createdParticipant.Dto())
+}
+
+func (a *App) participantFinished(w http.ResponseWriter, r *http.Request) {
+	startNumber, err := strconv.ParseInt(mux.Vars(r)["startNumber"], 10, 64)
+	respondWithClientError(w, err)
+
+	p, err := core.FetchParticipantByStartNumber(a.DB, int(startNumber))
+	respondWithServerError(w, err)
+
+	p, err = p.Finish(a.DB)
+	respondWithServerError(w, err)
+
+	respondWithDto(w, p.Dto())
 }
---
+++ server/routes.go
@@ -0,0 +1,10 @@
+package main
+
+func (a *App) initializeRoutes() {
+	a.Router.HandleFunc("/segments", a.createSegment).Methods("POST")
+	a.Router.HandleFunc("/segments", a.fetchAllSegment).Methods("GET")
+	a.Router.HandleFunc("/groups", a.createGroup).Methods("POST")
+	a.Router.HandleFunc("/groups/active", a.startActiveGroup).Methods("PUT")
+	a.Router.HandleFunc("/participants", a.registerParticipant).Methods("POST")
+	a.Router.HandleFunc("/participants/{startNumber}", a.participantFinished).Methods("POST")
+}
